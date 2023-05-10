6696f8f8afe197a62d5ed68b50989fd990bcd966
Hanmai001 <giahanmai3110@gmail.com>
2023-05-10 18:17:19 +0700

    add delete files for post-house layout

--- src/controllers/landlord.controller.js
+++ src/controllers/landlord.controller.js
@@ -3,12 +3,26 @@ const getPostHousePage = async (req, res) => {
     res.render("vwLandlord/post-house")
 }
 const getHouseManagementPage = async (req, res) => {
-    let {page} = req.query;
+    let { page } = req.query;
     if (!page) page = 1;
-    const {houses, pages} = await getAllHousesOfLandlord(1);
-    console.log(pages, page)
+    const { houses, pages } = await getAllHousesOfLandlord(1);
+    //console.log(pages, page)
     const result = await getLandlordHouseListModel(1, 5, page - 1);
-    return res.render("vwLandlord/house-management", { page: page ? parseInt(page) : 1, pages: parseInt(pages), houses: result})
+    for (let house of result) {
+        const date = new Date(house.NgayDang);
+        const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const vietnameseDate = date.toLocaleDateString('vi-VN', options);
+        house.NgayDang = vietnameseDate;
+        let str = house.Gia.toString();
+        let result = '';
+        while (str.length > 3) {
+            result = '.' + str.slice(-3) + result;
+            str = str.slice(0, -3);
+        }
+        result = str + result;
+        house.Gia = result;
+    }
+    return res.render("vwLandlord/house-management", { page: page ? parseInt(page) : 1, pages: parseInt(pages), houses: result })
 }
 const getManageAppointmentPage = async (req, res) => {
     return res.render("vwLandlord/manage-appointment")
--- src/models/house.model.js
+++ src/models/house.model.js
@@ -3,12 +3,11 @@ import db from "../config/db.config";
 
 const addHouseModel = async (data) => {
     const { cities, districts, wards, type_house, status, detailed_address, title, description, utilities, area, num_people, price, water, electricity } = data;
-    console.log(cities, districts, wards)
     const [id] = await db('tindangtro').insert(
         {
             Ten: title,
             DiaChi: detailed_address + ', ' + wards + ', ' + districts + ', ' + cities,
-            TrangThaiKiemDuyet: 1,
+            TrangThaiKiemDuyet: "Chờ xác nhận",
             Gia: price,
             MoTa: description,
             SoNguoi: parseInt(num_people),
@@ -21,8 +20,8 @@ const addHouseModel = async (data) => {
     return id;
 }
 const getAllHousesOfLandlord = async (idUser) => {
-    const res =  await db('tindangtro').select('*');
-    return {houses: res, pages: Math.ceil(res.length / 5)};
+    const res = await db('tindangtro').select('*');
+    return { houses: res, pages: Math.ceil(res.length / 5) };
 }
 const getLandlordHouseListModel = async (idUser, limit, offset) => {
     const result = await db('tindangtro')
