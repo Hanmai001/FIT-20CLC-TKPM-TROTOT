13ac692bc17b361cec0ebc5bc4e878d578c3e622
txquang2202 <tranxuanquang81@gmail.com>
2023-05-10 16:52:54 +0700

    main_page -> home
    Co-Authored-By: HQDuy47 <72744215+HQDuy47@users.noreply.github.com>

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,4 @@
 import initHouseRoute from './house';
-import initLandlordRoute from './landlord';
 import initTenantRoute from './tenant';
 import initDetailsRoute from './details_house';
 import initListRoute from './list-houses';
@@ -7,13 +6,12 @@ import initUtilityRoute from './utility';
 
 export default function (app) {
   app.use("/api", initUtilityRoute);
-  app.use("/landlord", initLandlordRoute);
   app.use("/tenant", initTenantRoute);
   app.use("/house", initHouseRoute);
   app.use("/details/:id", initDetailsRoute);
   app.use("/list", initListRoute);
   app.use("/", (req, res, next) => {
-    res.render("vwGuest/guest-main");
+    res.render("home");
   });
   app.use(function (req, res, next) {
     res.render("404", { layout: false });
