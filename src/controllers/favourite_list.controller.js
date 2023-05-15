db89dbc78f1441ce57f3029f8c40846f262f4a9d
Hanmai001 <giahanmai3110@gmail.com>
2023-05-15 21:38:05 +0700

    Chỉnh thêm xóa yêu thích

--- src/controllers/favourite_list.controller.js
+++ src/controllers/favourite_list.controller.js
@@ -2,12 +2,14 @@ import { addFavouritePostModel, deleteFavouritePostModel } from "../models/favou
 
 const addFavouritePost = async (req, res) => {
     const { idPost } = req.body;
+    console.log(req.body)
     const idUser = res.locals.user.id;
     await addFavouritePostModel(idPost, idUser);
     res.redirect('/');
 }
 const deleteFavouritePost = async (req, res) => {
     const idPost = req.params.id;
+    console.log(req.body)
     const idUser = res.locals.user.id;
     await deleteFavouritePostModel(idPost, idUser);
     res.redirect('/');
